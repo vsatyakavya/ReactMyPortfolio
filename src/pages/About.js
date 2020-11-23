397d4b2ee2c858dda6cd718fc0ca32da24370603
satya kavya <satyakavya24@gmail.com>
2020-11-23 07:11:59 -0600

    edit about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -14,14 +14,14 @@ function About() {
 
                     <div class="row">
                         <div class="col-md-12">
-                            <img src="kavya.jpg" class=" img-fluid rounded mx-auto d-block rounded-circle" alt="kavya"
+                            <img src="kavya.jpg" class=" img-fluid rounded mx-auto d-block rounded-circle" alt="satyakavya"
  />
                                 <h5>Satya kavya</h5>
                                 <p>I have done my under graduation in computer Science in India. I have done my college project in
                                 vb.net about my college.
                                 I have 1 year experience as a computer trainer. I'm a student of UT Austin coding
                                 bootcamp.
-                            My dream is to work in software industry. My goal is to become a great web developer.</p>
+                                My dream is to work in software industry. My goal is to become a great web developer.</p>
                                 <p>I like to play ping pong, carroms and board games like sequence. </p>
                                 <li><strong>Resume:   </strong> <a href="Assets/resume.pdf">satyakavyavarri</a></li>
                                 <li><strong>Email Address:</strong> <a href="mailto:satyakavya24@gmail.com">satyakavya24@gmail.com</a></li>
