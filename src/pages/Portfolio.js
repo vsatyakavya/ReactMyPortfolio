5a02f48aef63fff421d87fc13f9ca6b15b3b9a3e
satya kavya <satyakavya24@gmail.com>
2020-11-23 07:13:11 -0600

    add screenshots of projects to portfolio page

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -2,7 +2,25 @@ import React from "react"
 
 function Portfolio() {
     return (
-      <p>portfolio</p>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <h1>Portfolio</h1>
+                    <img src="Assets/login.png" class="img-fluid" alt="login" />
+                    <img src="Assets/petform.png" class="img-fluid" alt="petform" />
+                    <img src="Assets/videos.png" class="img-fluid" alt="videos page" /><hr />
+                    <img src="Assets/gmoh.png" class="img-fluid" alt="welcome page" />
+                    <img src="Assets/mapandplaces.png" class="img-fluid" alt="mapandplaces" /><hr />
+                    <img src="Assets/MyTeam.png" class="img-fluid" alt="team profiles" /><hr />
+                    <h1>QUIZ GAME</h1>
+                    <img src="Assets/quiz.png" class="img-fluid" alt="quiz"/><hr />
+                        <img src="Assets/weather.png" class="img-fluid" alt="weather dashboard" /><hr />
+                        <img src="Assets/passwordGenerator.png" class="img-fluid" alt="password Generator" /><hr />
+                        <img src="Assets/dayplanner.png" class="img-fluid" alt="mypicture" /><hr />
+                
+            </div>
+                </div>
+            </div>
     )
 
 }
