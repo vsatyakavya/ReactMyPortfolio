3b34f0857909fc51d41aeedf86cd03add19f7181
satya kavya <satyakavya24@gmail.com>
2020-11-24 22:04:57 -0600

    add image css effects

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -6,13 +6,13 @@ import videos from '../Assets/videos.png';
 import gmoh from '../Assets/gmoh.png'; 
 
 import mapandplaces from '../Assets/mapandplaces.png'; 
-import MyTeam from '../Assets/MyTeam.png'; 
+import employeeList from '../Assets/employeelist.png'; 
 
 import quiz from '../Assets/quiz.png'; 
 import weather from '../Assets/weather.png'; 
 
 import passwordGenerator from '../Assets/passwordGenerator.png'; 
-import dayplanner from '../Assets/dayplanner.png'; 
+import graph from '../Assets/graph.png'; 
 import Row from "../components/Row"
 import Container from "../components/Container"
 import Col from "../components/Col"
@@ -21,7 +21,9 @@ const styles ={
     img:{
         height:250,
         width:400,
-        padding:10}
+        padding:10,
+        borderRadius : 10
+    }
 }
 
 
@@ -36,12 +38,12 @@ function Portfolio() {
                 <img src={videos} class="img-fluid" alt="videos page" style={styles.img} />
                 <img src={gmoh} class="img-fluid" alt="welcome page"style={styles.img} />
                 <img src={mapandplaces} class="img-fluid" alt="mapandplaces" style={styles.img} />
-                <img src={MyTeam} class="img-fluid" alt="team profiles"style={styles.img}  />
-                <h1 >QUIZ GAME</h1>
-                <img src={quiz} class="img-fluid" alt="quiz" /><hr />
-                <img src={weather} class="img-fluid" alt="weather dashboard" /><hr />
-                <img src={passwordGenerator} class="img-fluid" alt="password Generator" /><hr />
-                <img src={dayplanner} class="img-fluid" alt="mypicture" /><hr />
+                <img src={employeeList} class="img-fluid" alt="login" style={styles.img} />
+                <img src={graph} class="img-fluid" alt="login" style={styles.img} />
+
+                <img src={quiz} class="img-fluid" alt="quiz" style={styles.img}/>
+                <img src={weather} class="img-fluid" alt="weather dashboard" style={styles.img}/>
+                <img src={passwordGenerator} class="img-fluid" alt="password Generator"style={styles.img} />
                 
             </Col>
         </Row>
