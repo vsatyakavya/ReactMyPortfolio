278687e9b1b7d6ddd28e0d000ad05f90381075fd
satya kavya <satyakavya24@gmail.com>
2020-11-24 21:06:52 -0600

    add projects

--- src/components/ProjectContainer/index.js
+++ src/components/ProjectContainer/index.js
@@ -8,32 +8,40 @@ const projects =[
  deployed : "https://fur-family-34.herokuapp.com/",
  github :"https://github.com/vsatyakavya/FurFamily"
 },
-{
-    id : 2,
-    title :"Employee Management system",
-    deployed :"https://drive.google.com/file/d/1OQMRRwzNPcQeWAARzk0grEq9WAVYsfXl/view",
-    github :"https://github.com/vsatyakavya/Employee_Tracker"
- },
+
 
 
  {
-    id : 3,
+    id : 2,
     title :"GetMeOutOfHere",
     deployed :"https://drop-g.github.io/GetMeOutOfHereApp/",
     github :"https://github.com/vsatyakavya/GetMeOutOfHereApp"
  },
  {
-    id : 4,
+    id : 3,
     title :"Quiz Game",
     deployed :"https://vsatyakavya.github.io/QuizGame.github.io/.",
     github :"https://github.com/vsatyakavya/QuizGame.github.io"
  },
  {
-    id : 5,
+    id : 4,
     title :" Password Generator",
     deployed :"https://vsatyakavya.github.io/PasswordGenerator.github.io/",
     github :"https://github.com/vsatyakavya/PasswordGenerator.github.io"
+ },
+ {
+    id :5,
+    title :"Budget Tracker",
+    deployed:"https://whispering-mesa-23895.herokuapp.com/",
+    github:"https://github.com/vsatyakavya/BudgetTracker"
+ },
+ { id :6,
+   title : "Employee Directory",
+   deployed: "https://vsatyakavya.github.io/EmployeeDirectory/",
+   github:"https://github.com/vsatyakavya/EmployeeDirectory"
+
  }
+
 
 ];
 
