d1b4e9186e320fd6b0eb5fff9e50f16ccf1178ae
satya kavya <satyakavya24@gmail.com>
2021-01-26 17:59:37 -0600

    apply display flex to align center the contact information

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,18 +1,27 @@
 import React from "react"
+import resume from "../../Assets/resume.pdf"
 
-const styles ={
-    foot :{
-        background: "gray",
-        display :"flex",
-        justifyContent : "center",
-        padding : 10
-    }
+const styles = {
+    foot: {
+        background: "#9198e5",
+        display: "flex",
+        justifyContent: "center",
+        
+        padding : 5
+    },
+
 }
-function Footer(){
+function Footer() {
     return (
-        <footer className="fixed-bottom" style ={styles.foot}>
-        <small>@copyright2020</small>
-    </footer>
+        <footer className="fixed-bottom" style={styles.foot}>
+            
+                <a href={resume} target="blank"><i class="fa fa-file fa-2x" style={{ color: "blue",marginRight:"20px" }}></i></a>
+        
+                <a href="https://github.com/vsatyakavya" target="blank"><i class="fa fa-github-square fa-2x" style={{ color: "black"  }}></i></a>
+                {/* <a href="your link here"> <i class="fa fa-dribbble fa-4x"></i></a> */}
+
+        </footer>
+
     )
 }
 export default Footer
